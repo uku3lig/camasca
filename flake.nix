3fc3cf84b119a995fa70ccb778d4f68c3f576bf5
uku <hi@uku.moe>
2024-08-28 23:09:05 +0200

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "collection of uku's modules and packages";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, flake-parts, ... } @ inputs: flake-parts.lib.mkFlake {inherit inputs;} {
+    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+  };
+}
