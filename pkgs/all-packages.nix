9655e7bad4fb619e35ad22ede3a68a9f3ba314d3
uku <hi@uku.moe>
2024-09-27 14:49:10 +0200

    chore(pkgs): remove wine-discord-ipc-bridge
    package was upstreamed

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -2,8 +2,4 @@ final: prev: {
   enigma = prev.callPackage ./enigma.nix {};
   vineflower = prev.callPackage ./vineflower.nix {};
   koi = prev.kdePackages.callPackage ./koi.nix {};
-
-  wine-discord-ipc-bridge = prev.callPackage ./wine-discord-ipc-bridge.nix {
-    inherit (prev.pkgsCross.mingw32) stdenv;
-  };
 }
