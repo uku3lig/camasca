fd017eee2fdbce44cbe57f3d72a34276ffad55d7
uku <hi@uku.moe>
2025-01-06 09:26:19 +0100

    koi: drop package
    upstreamed under kdePackages.koi

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -3,7 +3,6 @@ final: prev: {
   enigma = prev.callPackage ./enigma.nix { };
   jaspersoft-studio-community = prev.callPackage ./jaspersoft-studio-community.nix { };
   json2cdn = prev.callPackage ./json2cdn/package.nix { };
-  koi = prev.kdePackages.callPackage ./koi.nix { };
   openwebstart = prev.callPackage ./openwebstart.nix { };
   vineflower = prev.callPackage ./vineflower.nix { };
 }
