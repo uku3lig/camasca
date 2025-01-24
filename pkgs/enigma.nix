ef5052ebc6cd798f0847f51be44a462e17a1201b
uku <hi@uku.moe>
2025-01-24 15:16:21 +0100

    enigma: 2.5.0 -> 2.5.2

--- pkgs/enigma.nix
+++ pkgs/enigma.nix
@@ -8,13 +8,11 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   name = "enigma";
-  version = "2.5.0";
+  version = "2.5.2";
 
   src = fetchurl {
-    url =
-      with finalAttrs;
-      "https://maven.fabricmc.net/cuchaz/enigma-swing/${version}/enigma-swing-${version}-all.jar";
-    hash = "sha256-yOPPTKt96aRSbziYDBLBKqfLS2R9GeXgz5m2t1fgFHo=";
+    url = "https://maven.fabricmc.net/cuchaz/enigma-swing/${finalAttrs.version}/enigma-swing-${finalAttrs.version}-all.jar";
+    hash = "sha256-j4fqv6Ch7AZ9mBe+fDh0sXpkTY16EvQCUp4T5YhX8j0=";
   };
 
   dontUnpack = true;
@@ -27,7 +25,7 @@ stdenv.mkDerivation (finalAttrs: {
   installPhase = with finalAttrs; ''
     runHook preInstall
 
-    install -Dm644 $src $out/share/${name}.jar
+    install -Dm644 $src $out/share/${name}/${name}.jar
     makeWrapper ${jdk}/bin/java $out/bin/${name} --add-flags "-jar $out/share/${name}/${name}.jar"
 
     runHook postInstall
