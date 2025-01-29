953ded2042c7225141799ee9b402a3eb69f5556b
uku <hi@uku.moe>
2025-01-29 10:25:51 +0100

    json2cdn: drop
    upstreamed in nixpkgs

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -2,7 +2,6 @@ final: prev: {
   asus-numpad = prev.callPackage ./asus-numpad.nix { };
   enigma = prev.callPackage ./enigma.nix { };
   jaspersoft-studio-community = prev.callPackage ./jaspersoft-studio-community.nix { };
-  json2cdn = prev.callPackage ./json2cdn/package.nix { };
   openwebstart = prev.callPackage ./openwebstart.nix { };
   vineflower = prev.callPackage ./vineflower.nix { };
 }
